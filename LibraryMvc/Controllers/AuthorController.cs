0dcacbe8da7e25cab40cffbd9492e03a6b4c92dd
GovindSuccessive <govind.sharma@successive.tech>
2023-12-31 18:43:00 +0530

    Create Form of Author

--- LibraryMvc/Controllers/AuthorController.cs
+++ LibraryMvc/Controllers/AuthorController.cs
@@ -1,11 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryMvc.Data;
+using LibraryMvc.Models;
+using LibraryMvc.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryMvc.Controllers
 {
     public class AuthorController : Controller
     {
-        public IActionResult Index()
+        private readonly LibraryDbContext _libraryDbContext;
+
+        public AuthorController(LibraryDbContext libraryDbContext)
         {
+            _libraryDbContext = libraryDbContext;
+        }
+        [HttpGet]
+       
+        public async Task<List<Author>> Index()
+        {
+            return await _libraryDbContext.Author.ToListAsync();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddAuthorDto addAuthorDto)
+        {
+            if(addAuthorDto != null)
+            {
+                var author = new Author()
+                {
+                    AuthorName = addAuthorDto.AuthorName,
+                    AuthorEmail = addAuthorDto.AuthorEmail,
+                    Books = addAuthorDto.Books
+                };
+               await _libraryDbContext.Author.AddAsync(author);
+               await _libraryDbContext.SaveChangesAsync();
+               return RedirectToAction("Index");
+            }
             return View();
         }
     }
---
+++ LibraryMvc/Models/AddAuthorDto.cs
@@ -0,0 +1,13 @@
+﻿using LibraryMvc.Models.Domain;
+
+namespace LibraryMvc.Models
+{
+    public class AddAuthorDto
+    {
+        public string AuthorName { get; set; }
+
+        public string AuthorEmail { get; set; }
+
+        public virtual ICollection<Books>? Books { get; set; }
+    }
+}
